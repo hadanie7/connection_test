ac7e59dd28f1d606cc7ecd85d6df4e16b5eba0fb
JonatanHadas <jonatan@hadaso.net>
2017-08-13 18:08:45 +0300

    Add predictions to mouse control

--- graphics.py
+++ graphics.py
@@ -6,6 +6,7 @@ Created on Sun Aug 13 15:15:19 2017
 """
 
 import pygame
+from collections import deque
 
 import dummy
 
@@ -44,29 +45,27 @@ def mod(v1,mx,my):
     return tup2comp((x,y))
 
 class WorldDrawer:
-    def __init__(me, world):
-        me.world = world
-    def get_offset(me):
-        p = me.world.get_view_position()
+    def get_offset(me,world):
+        p = world.get_view_position()
         x = 0.5+0.5j
         return p - mod(p-x,w,h)-x
-    def wrld2scrn(me, v, pos = True):
+    def wrld2scrn(me,world, v, pos = True):
         if pos:
-            v -= me.get_offset()
+            v -= me.get_offset(world)
         v *= scale
         return rnd_tup(comp2tup(v))
-    def scrn2wrld(me, v, pos = True):
+    def scrn2wrld(me,world, v, pos = True):
         v = tup2comp(v)
         v /= scale
         if pos:
-            v += me.get_offset()
+            v += me.get_offset(world)
         return v
-    def draw(me, scr):
+    def draw(me, scr, world):
         scr.fill(bg_col)
         
                 
-        for obj in me.world.get_objs():
-            pos = me.wrld2scrn(obj.get_pos())
+        for obj in world.get_objs():
+            pos = me.wrld2scrn(world,obj.get_pos())
             cls = obj.get_class()
             if cls == 'st':
                 color = st_cols[obj.get_type()]
@@ -77,6 +76,13 @@ class WorldDrawer:
                 rad = int(scale*obj.get_radius())
                 pygame.draw.circle(scr,color,pos,rad)
 
+def world_steps(world, queues, my_cont):
+    while all([len(q)>0 for q in queues]):
+        world.step([q.popleft() for q in queues])
+    prediction = world.get_copy()
+    for i in xrange(len(queues[my_cont])):
+        prediction.step([q[i] if len(q)>i else 0+0j for q in queues])
+    return prediction
 
 def main():
     scr = pygame.display.set_mode((scale*(w+1),scale*(h+1)), pygame.FULLSCREEN)
@@ -86,10 +92,13 @@ def main():
     #should be changed later    
     #world = dummy.DummyWorld()
     world = World()
-    drawer = WorldDrawer(world)
+    drawer = WorldDrawer()
     
     pygame.mouse.set_visible(False)
     
+    queues = [deque() for i in xrange(world.get_controller_count())]
+    
+    my_cont = 0
     
     while True:
         for e in pygame.event.get():
@@ -100,10 +109,10 @@ def main():
                 if e.key == pygame.K_ESCAPE:
                     pygame.quit()
                     return
-        acc = tup2comp(pygame.mouse.get_rel()) * ms_spd
+        queues[my_cont].append(tup2comp(pygame.mouse.get_rel()) * ms_spd)
                 
-        world.step([acc])
-        drawer.draw(scr)
+        pred = world_steps(world,queues,my_cont)
+        drawer.draw(scr,pred)
         
         pygame.display.flip()
         
