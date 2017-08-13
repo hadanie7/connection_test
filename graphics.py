3179bf113073ae873d828ea6b4a30f06e6c6bba4
JonatanHadas <jonatan@hadaso.net>
2017-08-13 15:21:36 +0300

    Add graphics main loop

---
+++ graphics.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 13 15:15:19 2017
+
+@author: J&D
+"""
+
+import pygame
+
+pygame.init()
+
+scale = 32
+w,h = 19,14
+
+bg_col = (150,150,150)
+
+
+def main():
+    scr = pygame.display.set_mode((scale*(w+1),scale*(h+1)))
+    
+    clock = pygame.time.Clock()
+    
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                return
+        
+        scr.fill(bg_col)
+        
+        pygame.display.flip()
+        
+        clock.tick()
