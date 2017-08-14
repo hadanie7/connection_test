a2be470c3b1d8ca5f62843484cfc0683f751460e
hadanie7 <github@daniel.hadaso.net>
2017-08-15 00:28:03 +0300

    convert to new networking interface, change how timings are saved

--- comm.py
+++ comm.py
@@ -5,45 +5,56 @@ Created on Mon Aug 14 20:59:44 2017
 @author: D
 """
 
-import network
+from network import setup_conn
 import numpy as np
 from clock import Clock
-from tcp_stream import StreamReader, StreamWriter
+import time
+
+def get_timing_name():
+    try:    
+        with open('local/timing/counter.txt', 'r+') as f:
+            n = int(f.read())
+            f.seek(0)
+            f.write(str(n+1))
+            f.truncate()
+    except:
+        n = 10000+int(time.clock()*1000000) # choose random name
+    n = str(n).zfill(4)
+    return 'local/timing/dump{}.npz'.format(n)
 
 if __name__ == "__main__":
-    ans =  raw_input("Be Server? (y/n)")
-    if ans[0].lower() == 'y':
-        ip = None
-    else:
-        with open('local/con_ip.txt', 'r') as f:
-            ip = f.read()
-    s = network.get_socket(ip)
-    r = StreamReader(s)
-    w = StreamWriter(s)
-    
+    conn = setup_conn()
+      
     tms = []
     rec = []
     
     c = Clock()
     iii = 0
     STOP = 10*1000
+    STOP_ANYWAY = 20*1000
+    happy_ending = True
     while True:
         tms.append( c.get_time() )
         
         if iii < STOP:
-            w.write(str(iii))
+            conn.write(str(iii))
             
-        for mes in r.read():
+        for mes in conn.read():
             miii = int(mes)
             assert miii == len(rec)
             rec.append( c.get_time() )            
             
         iii += 1
         c.tick(0.001)
-        if len(tms) >= STOP and len(rec) >= STOP:
+        if (len(tms) >= STOP and len(rec) >= STOP) or len(tms) >= STOP_ANYWAY:
+            break
+        if not conn.are_you_OK():
+            happy_ending = False
             break
         
-    s.close()
+    conn.close()
     tms = np.array(tms)
     rec = np.array(rec)
-    np.savez('local/timing.npz', tms = tms, rec = rec)
+
+    np.savez(get_timing_name(), tms = tms, rec = rec,
+             happy_ending=happy_ending)
