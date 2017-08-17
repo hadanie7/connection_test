2e0accf40ef1f2780d12b01492a8b0d0295db52a
JonatanHadas <jonatan@hadaso.net>
2017-08-17 22:01:20 +0300

    Fix to new UDP version

--- udp_stream_v2.py
+++ udp_stream_v2.py
@@ -30,6 +30,8 @@ class UDPStreamRed:
     MSEP = '\n'
     
     ack_red_num = 1
+    num_lim = 20
+    ack_lim = 50
     def __init__(me, port, ip):
         me.addr = (ip,port)
         
@@ -62,12 +64,15 @@ class UDPStreamRed:
         except:
             me.log()
     def send_ack(me, nums):
-        txt = me.SEP.join([me.ACK]+[str(num) for num in nums])
-        for i in xrange(me.ack_red_num):
-            me.send(txt)
+        for i in xrange(0,len(nums),me.ack_lim):
+            txt = me.SEP.join([me.ACK]+[str(num) for num in nums[i:i+me.ack_lim]])
+            for i in xrange(me.ack_red_num):
+                me.send(txt)
     def resend(me):
-        txt = me.MSEP.join(str(num)+me.SEP+me.unack[num][0] for num in me.unack)
-        me.send(txt)
+        nums = list(me.unack)
+        for i in xrange(0,len(nums),me.num_lim):
+            txt = me.MSEP.join(str(num)+me.SEP+me.unack[num][0] for num in nums)
+            me.send(txt)
     def ack(me, num):
         if num in me.unack:
             me.unack.pop(num)
