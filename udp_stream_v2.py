ef1eb70776d75c1445a8719218a606fcb4b3ec70
JonatanHadas <jonatan@hadaso.net>
2017-08-18 01:10:04 +0300

    Fix bug in latest UDP version

--- udp_stream_v2.py
+++ udp_stream_v2.py
@@ -163,6 +163,7 @@ class UDPStreamGrp:
             me.l -= d
             me.txt = me.txt[me.sts[d]-me.sts[0]]
             me.sts = me.sts[d:]
+            me.n = N
             return False
             
             
