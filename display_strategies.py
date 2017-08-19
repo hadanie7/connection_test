1504ccca47e58097c09c4212a7e80dfb51da0b38
JonatanHadas <jonatan@hadaso.net>
2017-08-19 19:38:23 +0300

    Create smoothed display strategy

--- display_strategies.py
+++ display_strategies.py
@@ -34,12 +34,45 @@ class SimpleDelayed:
         while all([len(q)>0 for q in queues]):
             self.world.step([q.popleft() for q in queues])
         return self.world
+        
+def null_func():
+    pass
+        
+class SmoothedDelayed:
+    
+    def once_per_10(self,x):
+        if self.cnt>0:
+            self.cnt -= 1
+            return min(x,1)
+        else:
+            if x>1:
+                self.cnt = 10
+            return min(x,2)
+    def init_once(self):
+        self.cnt = 0
+        
+        
+    def __init__(self, world, func_type = 'simple'):
+        smooth_types = {
+                        'simple':lambda x: (min(x,5),null_func),
+                        'once_per_10':(self.once_per_10,self.init_once),
+                        }
+        self.world = world
+        self.smooth, init = self.smooth_types[func_type]
+        init()
+    def step(self, queues, my_cont):
+        x = min([len(q) for q in queues])
+        for i in xrange(self.smooth(x)):
+            self.world.step([q.popleft() for q in queues])
+        return self.world
 
 def get_disp_strat(world):
     with open('local/disp_strat.txt') as f:
-        tp = f.read()
-    cls = {'SimpleExtrapolate':SimpleExtrapolate, 'SimpleDelayed':SimpleDelayed}[tp]
-    return cls(world)
+        params = f.read().split()
+    tp = params[0]
+    params = params[1:]
+    cls = {'SimpleExtrapolate':SimpleExtrapolate, 'SimpleDelayed':SimpleDelayed, 'SmoothedDelayed':SmoothedDelayed}[tp]
+    return cls(world, *params)
 #    if tp == 'SimpleExtrapolate':
 #        return SimpleExtrapolate(world)
 #    elif tp == 'SimpleDelayed':
